db8633553814e14c1db7b56aa5c548b16e008970
Барахоева Хадишат <hadibar25@gmail.com>
2026-04-20 11:41:57 +0700

    Создана ветка lab03

--- backend/schemas/item_schema.py
+++
@@ -1,9 +0,0 @@
-from apiflask import Schema
-from apiflask.fields import Integer, String, List, Nested
-
-class ItemSchema(Schema):
-    id = Integer()
-    name = String()
-
-class ItemsOutSchema(Schema):
-    items = List(Nested(ItemSchema))
--- backend/schemas/status_schema.py
+++
@@ -1,8 +0,0 @@
-from apiflask import Schema
-from  apiflask.fields import String, Integer
-
-class StatusOutSchema(Schema):
-    status = String()
-    message = String()
-    service = String()
-    items_count = Integer()
